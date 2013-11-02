5f0339fc40a1a64cd43716ec5d8fe3f73650d861
John Weis <weis.john@gmail.com>
2013-11-01 23:51:17 -0400

    retrieving the projects

--- index.js
+++ index.js
@@ -2,16 +2,20 @@
 // kamino - a node.js powered cli for cloning lots of repos from Gitlab
 
 var commander = require('commander')
-  , fs = require('fs')
   , colors = require('colors')
+  , url = require('url')
+  , request = require('request')
   ;
 
-// setup 
+
+// setup
 commander.version('0.0.1')
-	.option('-h, --host [IP || DNS]', 'The hostname for Gitlab')
+	.option('-h, --host [IP || DNS]', 'The IP address or hostname for Gitlab')
+	.option('-s, --secure', 'Use https instead')
 	.option('-t, --token [token]', 'Your account\'s API token')
 	.option('-d, --dir [dir]', 'The directory to clone projects into')
 	.parse(process.argv);
+
 
 // validation
 ["host","token","dir"].forEach(function(k) {
@@ -20,3 +24,44 @@ commander.version('0.0.1')
 		process.exit(1);
 	}
 });
+
+
+// resolve the url
+commander.host = "http" + (commander.secure ? "s" : "") + "://" + commander.host + "/api/v3/";
+var gitlab = url.parse(commander.host);
+if (!gitlab.host) {
+	console.error("It appears that `host` is invalid".red);
+	process.exit(1);
+}
+
+
+// setup request
+var r = request.defaults({ qs : {
+	"private_token" : commander.token,
+	"per_page" : 100
+}, json : true });
+
+
+// error handling
+function handle(cb) {
+	return function(err, response, json) {
+
+		if (err && err.code === "ENOTFOUND") {
+			console.error('Couldn\'t find Gitlab.'.red);
+		} else if (response.body && response.body.error) {
+			console.error(response.body.error.red);
+		} else {
+			return cb(err || response, json);
+		}
+
+		process.exit(1);
+	}
+}
+
+
+// find projects
+var projects = [];
+r.get(gitlab.resolve("projects"), handle(function(err, projects) {
+	console.log(projects.length);
+}));
+
